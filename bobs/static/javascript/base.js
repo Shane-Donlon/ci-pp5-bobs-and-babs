64f01065de934435530fe7164ecfb6f43b8d85a6
Shane-Donlon <shane.donlon@live.com>
2024-04-08 14:31:11 +0100

    style add: header sticky with box shadow

--- bobs/static/javascript/base.js
+++ bobs/static/javascript/base.js
@@ -25,3 +25,21 @@ window.addEventListener("DOMContentLoaded", () => {
     navUl.setAttribute("aria-hidden", "");
   }
 });
+
+const primaryHeader = document.querySelector("header");
+const scrollWatcher = document.createElement("div");
+
+scrollWatcher.setAttribute("data-scroll-watcher", "");
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver(
+  (entries) => {
+    primaryHeader.classList.toggle(
+      "box-shadow-show",
+      !entries[0].isIntersecting
+    );
+  },
+  { rootMargin: "50px 0px 0px 0px" }
+);
+
+navObserver.observe(scrollWatcher);
