6203488e7e899b1961ff67eca1c16c251b4895bd
Shane-Donlon <shane.donlon@live.com>
2024-05-03 12:47:54 +0100

    fix update url routing

--- bobs/products/urls.py
+++ bobs/products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ProductsPageDefaultView.as_view(), name="products-default"),
+    path("", views.ProductsPageDefaultView.as_view(), name="products"),
 ]
