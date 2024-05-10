93028f5fe42db1cbc753c76292cafbccd9838eae
Shane-Donlon <shane.donlon@live.com>
2024-05-10 14:08:43 +0100

    Feat add Order model

--- bobs/store/models.py
+++ bobs/store/models.py
@@ -28,3 +28,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
