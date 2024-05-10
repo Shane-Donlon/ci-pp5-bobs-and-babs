b949603adda8e6c3eeab6dd33b016a865cb42c08
Shane-Donlon <shane.donlon@live.com>
2024-05-10 14:18:14 +0100

    Feat add Order Items model

--- bobs/store/models.py
+++ bobs/store/models.py
@@ -15,7 +15,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=60, null=True, blank=False)
     registration_date = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=False, auto_now_add=False)
-    
+
     def __str__(self):
         return self.user
     
@@ -39,3 +39,12 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.transaction_id)
+
+class OrderItems(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
