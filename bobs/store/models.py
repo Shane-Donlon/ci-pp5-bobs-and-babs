2b337552ff9b2a4aacded43df7a47a1aab916439
Shane-Donlon <shane.donlon@live.com>
2024-05-10 14:10:06 +0100

    fix add return str to Order model

--- bobs/store/models.py
+++ bobs/store/models.py
@@ -34,3 +34,6 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.transaction_id)
