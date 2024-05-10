c75fd87e09442e4fd35bc0ab6de2459d8b6ec51c
Shane-Donlon <shane.donlon@live.com>
2024-05-10 14:26:18 +0100

    Feat update Customer registered admin

--- bobs/store/admin.py
+++ bobs/store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Customer, Order, OrderItems, Product, ShippingInformation
+
 # Register your models here.
+
+@admin.register(Customer)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "phone", "email", "registration_date"]
+    search_fields = ["first_name", "email", "phone"]
