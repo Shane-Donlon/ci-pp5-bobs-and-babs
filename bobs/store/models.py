4591622e75ddb3f5eb154d56f21b9c9479584eae
Shane-Donlon <shane.donlon@live.com>
2024-05-10 14:08:18 +0100

    feat update Customer Model address

--- bobs/store/models.py
+++ bobs/store/models.py
@@ -5,10 +5,20 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
     email = models.EmailField(max_length=200, null=True)
+    address = models.CharField(max_length=300, null=True, blank=False)
+    town = models.CharField(max_length=60, null=True, blank=False)
+    county = models.CharField(max_length=60, null=True, blank=False)
+    eircode = models.CharField(max_length=8, null=True, blank=False,)
+    phone = models.CharField(max_length=60, null=True, blank=False)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=False, auto_now_add=False)
+    
     def __str__(self):
-        return self.name
+        return self.user
+    
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
