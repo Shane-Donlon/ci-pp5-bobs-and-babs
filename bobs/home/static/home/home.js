f4865ca81728cc54c3712d0bc8b149b91f6155c0
Shane-Donlon <shane.donlon@live.com>
2024-05-14 16:52:56 +0100

    script add: about us link, and scroll margin

---
+++ bobs/home/static/home/home.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", (e) => {
+  // if the user is on the home page update the href attribute of the about us link as to avoid a page refresh
+  let aboutUsLink = document.querySelector("#about-us-link");
+  aboutUsLink.setAttribute("href", "#about-us");
+  // set the css variable for the nav height so that the nav does not cover the content
+  let navWrapper = document.querySelector(".nav-wrapper");
+  let height = navWrapper.getBoundingClientRect().height;
+  document.body.style.setProperty("--nav-height", `${height}px`);
+});
