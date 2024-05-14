ac40526e8fe826bc3ddcc38ee26f42d98e2cab01
Shane-Donlon <shane.donlon@live.com>
2024-05-14 13:23:28 +0100

    fix - moved product models to product app

--- bobs/products/admin.py
+++ bobs/products/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from .models import Customer, Order, OrderItems, Product, ShippingInformation
+
 # Register your models here.
+
+@admin.register(Customer)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "phone", "email", "registration_date"]
+    search_fields = ["first_name", "email", "phone"]
+
+@admin.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price"]
+    search_fields = ["name", "sku", ]
+
+@admin.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["customer", "transaction_id"]
+    search_fields = ["customer", "transaction_id", ]
+
+@admin.register(OrderItems)
+
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ["product", "quantity"]
+    search_fields = ["product", "quantity"]
+
+@admin.register(ShippingInformation)
+
+class ShippingInformationAdmin(admin.ModelAdmin):
+    list_display = ["customer", "order", "phone"]
+    search_fields = ["customer", "phone"]
