c73d3d1d3af2d0eefc07e49479c5a6099cdd893b
Shane-Donlon <shane.donlon@live.com>
2024-05-16 15:14:28 +0100

    Fix cart page view update

--- bobs/cart/views.py
+++ bobs/cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from products.models import Order, OrderItems, Product
 
 # Create your views here.
 
@@ -7,6 +8,15 @@ from django.views import View
 class CartPageDefaultView(View):
 
     def get(self, request):
+        if(request.user.is_authenticated):
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            items = order.orderitems_set.all()
+            context = {"items": items, "order": order, "created": created,}
+            return render(request, "cart/cart.html", context)
 
-        context = {"max_quantity": 10, }
-        return render(request, "cart/cart.html", context)
+        else:
+            items = []
+            order = {"get_cart_total": 0, "get_cart_items": 0}
+            context = {"max_quantity": 10, }
+            return render(request, "cart/cart.html", context)
