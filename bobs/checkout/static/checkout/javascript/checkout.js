4d4e4cb784e40c978f33a906d44cc08badeadbf5
Shane-Donlon <shane.donlon@live.com>
2024-05-16 15:10:14 +0100

    Script add: update Delivery fee on input

--- bobs/checkout/static/checkout/javascript/checkout.js
+++ bobs/checkout/static/checkout/javascript/checkout.js
@@ -1,6 +1,25 @@
-window.addEventListener("resize", (e) => {
-  let section = document.querySelector("section");
-  let left = section.getBoundingClientRect().left;
-  console.log(left);
-  //   document.body.style.setProperty("--left", left + "px");
+let delivery = document.querySelector(".delivery-charge-price > data");
+let deliverValue = delivery.value;
+let deliveryPrice = parseFloat(deliverValue);
+
+let radioBtns = document.querySelectorAll(".delivery-method-radio");
+let deliveryPriceElement = document.querySelector(".delivery-charge-price");
+
+radioBtns.forEach((btn) => {
+  // updates the delivery price based on the radio button selected
+  btn.addEventListener("input", (e) => {
+    let btnSelected = e.target.value;
+    if (btnSelected === "collection") {
+      let priceForDelivery = 0;
+      delivery.value = priceForDelivery;
+
+      deliveryPriceElement.textContent = `€${priceForDelivery.toFixed(2)}`;
+      return;
+    }
+    if (btnSelected === "delivery") {
+      delivery.value = deliveryPrice;
+      deliveryPriceElement.textContent = `€${deliveryPrice.toFixed(2)}`;
+      return;
+    }
+  });
 });
