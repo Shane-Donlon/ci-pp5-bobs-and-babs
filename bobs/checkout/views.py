5859846d898ccd4c7a102857752dc66a7b3710ab
Shane-Donlon <shane.donlon@live.com>
2024-05-16 15:15:12 +0100

    feat update checkout view context add

--- bobs/checkout/views.py
+++ bobs/checkout/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.views import View
+from products.models import Order, OrderItems, Product
 
 
 class CheckoutPage(View):
 
     def get(self, request):
-        return render(request, "checkout/checkout.html",)
+        if(request.user.is_authenticated):
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            items = order.orderitems_set.all()
+            context = {"items": items, "order": order, "created": created,"customer":customer, }
+            return render(request, "checkout/checkout.html", context)
+
+        else:
+            items = []
+            order = {"get_cart_total": 0, "get_cart_items": 0}
+            context = {"max_quantity": 10, }
+            return render(request, "checkout/checkout.html", context)
+
+
+
