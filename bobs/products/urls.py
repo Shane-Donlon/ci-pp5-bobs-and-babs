a8046f1fe48134c25a63488592cf133a03db48d7
Shane-Donlon <shane.donlon@live.com>
2024-05-17 15:38:57 +0100

    feat add url product detail and add to cart

--- bobs/products/urls.py
+++ bobs/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.ProductsPageDefaultView.as_view(), name="products"),
-    path("<slug:slug_field>", views.ProductDetailView.as_view(), name="product_detail"),
+    path("<slug:slug_field>", views.ProductDetailView.as_view(), name="product-detail"),
+    path("<slug:slug_field>/cart/add", views.addToCart.as_view(), name="add-to-cart"),
 ]
