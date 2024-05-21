6b0a7784e7c90391f2fd9359a9e703af56e11356
Shane-Donlon <shane.donlon@live.com>
2024-05-21 16:59:54 +0100

    Feat: cart area and total update on el remove

--- bobs/cart/static/cart/javascript/cart.js
+++ bobs/cart/static/cart/javascript/cart.js
@@ -1,6 +1,6 @@
 let allBtns = document.querySelectorAll(".del-btn");
 let links = document.querySelectorAll("[data-slug]");
-
+let total = document.querySelector(".total-cart");
 allBtns.forEach((btn, index) => {
   btn.addEventListener(
     "click",
@@ -22,8 +22,16 @@ allBtns.forEach((btn, index) => {
           createNotification(data.success, "success");
           let getRequest = makeRequest("", "GET");
           getRequest.then((data) => {
-            console.log(data.order);
+            let selectedEl = document.querySelectorAll(`.cart-grid > .${slug}`);
+            selectedEl.forEach((el) => {
+              let transitionTime = "250ms";
+              transitionAndRemoveElement(el, transitionTime);
+            });
+
+            let cartTotal = JSON.parse(data.order)[0].fields.cart_total;
+            total.innerHTML = `<data value="${cartTotal}">€${cartTotal}</data>`;
           });
+
           return;
         }
         if (data.error) {
@@ -44,4 +52,28 @@ function generateOrder(productId, quantity, actionType, transactionId) {
   cartBody["transactionId"] = transactionId;
   let bodyJson = JSON.stringify({ cart: cartBody });
   return bodyJson;
+}
+
+function transitionAndRemoveElement(el, transitionTime) {
+  el.style.transition = `height ${transitionTime} linear`;
+
+  el.style.opacity = "0";
+  let elHeight = el.offsetHeight;
+
+  el.style.height = `${elHeight}px`;
+
+  el.offsetHeight;
+
+  el.style.height = "0px";
+
+  el.addEventListener(
+    "transitionend",
+    function transitionEnd(event) {
+      if (event.propertyName === "height") {
+        el.remove();
+        el.removeEventListener("transitionend", transitionEnd, false);
+      }
+    },
+    false
+  );
 }
