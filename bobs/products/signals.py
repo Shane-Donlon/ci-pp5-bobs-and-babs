95c6eb36489277398344146f8886ad752930a37d
Shane-Donlon <shane.donlon@live.com>
2024-05-21 15:05:58 +0100

    feat update transaction id move to signal

--- bobs/products/signals.py
+++ bobs/products/signals.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -11,3 +13,9 @@ def update_cart_total(sender, instance, **kwargs):
     if cart_total != instance.cart_total:
         instance.cart_total = cart_total
         instance.save(update_fields=['cart_total'])
+
+@receiver(post_save, sender=Order)
+def update_transaction_id(sender, instance, **kwargs):
+    if not instance.transaction_id:
+        instance.transaction_id = str(uuid.uuid4())
+        instance.save(update_fields=['transaction_id'])
