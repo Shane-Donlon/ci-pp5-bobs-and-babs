b70a53615125aada2df2d2ec3d03edc0dd94eb78
Shane-Donlon <shane.donlon@live.com>
2024-05-21 15:09:43 +0100

    feat update: cart url update slug add

--- bobs/cart/urls.py
+++ bobs/cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.CartPageDefaultView.as_view(), name="cart"),
+    path("update/<slug:slug_field>/", views.updateCart.as_view(), name="update_cart"),
 ]
