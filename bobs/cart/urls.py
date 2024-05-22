b44818e5937e130f99a630df37d6f9752c904ee0
Shane-Donlon <shane.donlon@live.com>
2024-05-22 19:53:56 +0100

    refactor remove unused url

--- bobs/cart/urls.py
+++ bobs/cart/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.CartPageDefaultView.as_view(), name="cart"),
-    path("update/<slug:slug_field>/", views.updateCart.as_view(), name="update_cart"),
 ]
