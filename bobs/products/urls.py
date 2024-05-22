e2acdc27b8f530f34baee04f644a9b0501515fed
Shane-Donlon <shane.donlon@live.com>
2024-05-22 19:47:10 +0100

    feat add url cart item removal

--- bobs/products/urls.py
+++ bobs/products/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.ProductsPageDefaultView.as_view(), name="products"),
     path("<slug:slug_field>", views.ProductDetailView.as_view(), name="product-detail"),
-    path("<slug:slug_field>/cart/add", views.addToCart.as_view(), name="add-to-cart"),
+    path("<slug:slug_field>/cart/add", views.AddToCart.as_view(), name="add-to-cart"),
+    path("<slug:slug_field>/cart/remove", views.RemoveFromCart.as_view(), name="remove-from-cart"),
 ]
