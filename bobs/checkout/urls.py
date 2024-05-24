d9129cc4a0308bb35ccda78b10664c936bf8961b
Shane-Donlon <shane.donlon@live.com>
2024-05-24 12:23:06 +0100

    feat add charge and success urls

--- bobs/checkout/urls.py
+++ bobs/checkout/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.CheckoutPage.as_view(), name='checkout'),
+    path("charge/<str:transaction_id>/", views.Charge.as_view(), name="charge"),
+    path("order-success/", views.OrderSuccess.as_view(), name="order_complete"),
 ]
