e1fa8ebfb02ebba664e930ad4caecc55337ccdd3
Shane-Donlon <shane.donlon@live.com>
2024-05-29 15:11:46 +0100

    feat add counties.py

---
+++ bobs/checkout/counties.py
@@ -0,0 +1,9 @@
+# array of locations in ireland
+
+COUNTIES = ["","Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork",
+            "Derry", "Donegal", "Down", "Dublin",
+            "Fermanagh", "Galway", "Kerry", "Kildare", "Kilkenny", "Laois",
+            "Leitrim", "Limerick", "Longford", "Louth",
+            "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon",
+            "Sligo", "Tipperary", "Tyrone", "Waterford",
+            "Westmeath", "Wexford", "Wicklow"]
