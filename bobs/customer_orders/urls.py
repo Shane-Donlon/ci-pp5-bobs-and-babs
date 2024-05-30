de47a87414a1c8a2d6bd9c39cfa0da9e72b6a181
Shane-Donlon <shane.donlon@live.com>
2024-05-30 16:50:54 +0100

    fix update view names

--- bobs/customer_orders/urls.py
+++ bobs/customer_orders/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GeneralPageDefaultView.as_view(), name='orders'),
+    path('', views.GetAllOrdersView.as_view(), name='orders'),
+    path('view/<str:transaction_id>/', views.GetSingularOrder.as_view(), name='individual_order'),
 ]
