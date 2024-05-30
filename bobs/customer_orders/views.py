038605782fc093e4ee1c3d487ed7b6deed5cba5b
Shane-Donlon <shane.donlon@live.com>
2024-05-30 16:26:30 +0100

    feat add item.total price to order page

--- bobs/customer_orders/views.py
+++ bobs/customer_orders/views.py
@@ -1,21 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.http import require_GET
-from django_tables2 import RequestConfig, SingleTableView
+from django_tables2 import RequestConfig
 from products.models import Order, OrderItems
 
 from .tables import orderTable
 
 
 @method_decorator([require_GET, login_required], name='dispatch')
-class GetOrdersView(SingleTableView,View):
-
+class GetAllOrdersView(View):
     def get(self, request):
         customer = request.user.customer
-        orders = Order.objects.filter(customer=customer)
+        orders = Order.objects.filter(customer=customer).order_by('-date_ordered')
         table = orderTable(
                 orders,
                 template_name="django_tables2/bootstrap5-responsive.html")
@@ -30,16 +29,19 @@ class GetOrdersView(SingleTableView,View):
 
 
 @method_decorator([require_GET, login_required], name='dispatch')
-class GetOrder(View):
-
+class GetSingularOrder(View):
     def get(self, request, transaction_id):
-        order = Order.objects.get(transaction_id=transaction_id)
+        order = get_object_or_404(Order, transaction_id=transaction_id)
         items = order.orderitems_set.all()
+
         if not request.user.customer:
             raise PermissionDenied
 
         if request.user.customer != order.customer:
             raise PermissionDenied
+
+        for item in items:
+            item.total_price = item.product.price * item.quantity
 
         context = {
                 'order': order,
