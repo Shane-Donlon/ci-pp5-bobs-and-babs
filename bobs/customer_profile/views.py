2db80a634820dd42cb4a2425d56cff29685e5963
Shane-Donlon <shane.donlon@live.com>
2024-05-30 11:11:19 +0100

    feat udpate profile form dispalys username as name

--- bobs/customer_profile/views.py
+++ bobs/customer_profile/views.py
@@ -1,8 +1,9 @@
 import json
 
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
+from products.models import Customer
 
 from . import forms
 
@@ -10,18 +11,17 @@ from . import forms
 class ProfilePageView(View):
 
     def get(self, request):
-        initial_data = {
-    'user': request.user.customer.user.username,
-    'first_name': request.user.customer.first_name,
-    'last_name': request.user.customer.last_name,
-    'email': request.user.customer.email,
-    'address': request.user.customer.address,
-    'town': request.user.customer.town,
-    'county': request.user.customer.county,
-    'eircode': request.user.customer.eircode,
-    'phone': request.user.customer.phone,
-}
-        form = forms.ProfileForm(initial=initial_data)
+
+        customer = get_object_or_404(Customer, user=request.user.customer.user_id)
+        form = forms.ProfileForm(instance=customer, initial={"user": request.user.customer})
         context = {"form": form}
-        print(request.user.customer)
         return render(request, "customer_profile/customer_profile.html",context)
+
+    def post(self, request):
+        post_data = json.loads(request.body)
+        form = forms.ProfileForm(post_data)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({"success": "Profile updated successfully"})
+
+        return JsonResponse({"error": form.errors})
