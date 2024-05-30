7859df74fb48991d9b500437ce5c54b285c7ddcc
Shane-Donlon <shane.donlon@live.com>
2024-05-30 12:03:43 +0100

    fix import shipping information model

--- bobs/checkout/forms.py
+++ bobs/checkout/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from products.models import Order, ShippingInformation
+from products.models import ShippingInformation
 
 from .counties import COUNTIES
 
