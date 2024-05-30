84d18a9a98f6ec204a8815ea7c63c5af104959c6
Shane-Donlon <shane.donlon@live.com>
2024-05-30 12:05:21 +0100

    feat add customer_portal app

---
+++ bobs/customer_portal/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.CustomerPortalDefaultView.as_view(), name='customer-portal'),
+
+]
---
+++ bobs/customer_portal/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.views import View
+
+
+class CustomerPortalDefaultView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        customer = request.user.customer
+        context =  {
+            "customer": customer}
+        return render(request, "customer_portal/customer_portal.html",context)
+
+
