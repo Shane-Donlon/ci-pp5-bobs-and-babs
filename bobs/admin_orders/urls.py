fc10379eff945e8e88829737cd960944f5d748ef
Shane-Donlon <shane.donlon@live.com>
2024-06-03 19:16:29 +0100

    feat add admin order urls

---
+++ bobs/admin_orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.AdminOrdersAllOrders.as_view(), name='admin_orders_view_all'),
+    path('<int:pk>/', views.AdminOrdersIndividualOrder.as_view(), name='admin_orders_view_individual'),
+]
