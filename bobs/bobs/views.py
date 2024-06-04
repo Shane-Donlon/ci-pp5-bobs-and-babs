c0d0be6254a275c8506c572b89bf20e7bf8cb671
Shane-Donlon <shane.donlon@live.com>
2024-06-04 17:11:23 +0100

    fix evert change frequency

--- bobs/bobs/views.py
+++ bobs/bobs/views.py
@@ -1,7 +1,10 @@
 """Views to handle errors"""
+
+
 from django.contrib.sitemaps import Sitemap
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.decorators.http import require_GET
 from products.models import Product
 
