7316d129c97756ed06fd42022796543b9c9ccf11
Shane-Donlon <shane.donlon@live.com>
2024-06-04 21:21:13 +0100

    fix pep8 udpate

--- bobs/bobs/views.py
+++ bobs/bobs/views.py
@@ -23,19 +23,24 @@ Disallow: /customer-portal/
 Sitemap: sd-ci-pp5-bobs-and-babs-5fa3ca5e7225.herokuapp.com/sitemap.xml
 """
 
+
 @require_GET
 def robots_txt_file(request):
     return HttpResponse(robots_txt_file_content, content_type="text/plain")
+
 
 @require_GET
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "404.html", status=404)
 
+
 @require_GET
 def handler500(request):
     """ Error Handler 500 - Internal Server Error """
     return render(request, "500.html", status=500)
+
+
 @require_GET
 def handler403(request, exception):
     """ Error Handler 403 - Access denied """
