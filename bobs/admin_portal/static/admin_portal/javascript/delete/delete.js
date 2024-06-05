5ab69420ed978a156695780e8fac43d0bf0defef
Shane-Donlon <shane.donlon@live.com>
2024-06-05 16:48:34 +0100

    fix catch error add

--- bobs/admin_portal/static/admin_portal/javascript/delete/delete.js
+++ bobs/admin_portal/static/admin_portal/javascript/delete/delete.js
@@ -1,14 +1,13 @@
 let deleteForm = document.querySelector("form");
 deleteForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  window.confirm("Are you sure you want to delete this product?");
-  if (window.confirm) {
+  if (window.confirm("Are you sure you want to delete this item?")) {
     let formData = new FormData(deleteForm);
     makeRequest(deleteForm.action, "DELETE", formData)
       .then((data) => {
         window.location.href = `${data.redirect}`;
       })
-      .catch((error) => {});
+      .catch((error) => {console.error(error);});
   } else {
     return;
   }
